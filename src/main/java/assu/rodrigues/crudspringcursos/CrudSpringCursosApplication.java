ecba896e79ecb173053a9e49bba0a8cfd735409c
assunaueny-rodrigues <assunauenyufersa@gmail.com>
2024-05-07 20:38:44 -0300

    initial commit

---
+++ src/main/java/assu/rodrigues/crudspringcursos/CrudSpringCursosApplication.java
@@ -0,0 +1,13 @@
+package assu.rodrigues.crudspringcursos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudSpringCursosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudSpringCursosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/assu/rodrigues/crudspringcursos/CrudSpringCursosApplicationTests.java
@@ -0,0 +1,13 @@
+package assu.rodrigues.crudspringcursos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudSpringCursosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
