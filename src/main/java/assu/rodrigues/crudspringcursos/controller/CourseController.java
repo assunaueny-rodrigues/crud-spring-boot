cc46917b6ff667f101254f0c323275388352876e
Assunaueny Rodrigues <assu.rodrigues@attornatus.com.br>
2024-07-09 21:43:19 -0300

    feat: criando endpoint de adicionar curso

--- src/main/java/assu/rodrigues/crudspringcursos/controller/CourseController.java
+++ src/main/java/assu/rodrigues/crudspringcursos/controller/CourseController.java
@@ -2,11 +2,19 @@ package assu.rodrigues.crudspringcursos.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 import java.util.List;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import assu.rodrigues.crudspringcursos.model.Course;
 import assu.rodrigues.crudspringcursos.repository.CourseRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
 
 
 @RestController
@@ -21,4 +29,10 @@ public class CourseController {
         return courseRepository.findAll();
     }
     
+
+    @PostMapping    
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Course add(@RequestBody Course course){
+        return courseRepository.save(course);
+    }
 }
